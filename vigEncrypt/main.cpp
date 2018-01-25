ce346f5e59ec95677eb6821651fe337a4bc3d9a1
matthew <matthewaerose@gmail.com>
2018-01-25 11:20:30 -0600

    add function to remove lower case letters.
    USES LAMBDAS!!

--- vigEncrypt/main.cpp
+++ vigEncrypt/main.cpp
@@ -17,6 +17,10 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <math.h>
+
+#include <cctype>
+
 
 //using namespace std;
 std::string plainTextFile = "/tmp/plaintext.txt";
@@ -35,6 +39,7 @@ bool writeFilesToTMP(void);
 
 std::string trim(const std::string &s);
 void removeSpaces(std::string &s);
+void removeLowercase(std::string &s);
 
 void encrypt_S(std::string plain, std::string key, std::string &output);
 void encrypt_L(std::string plain, std::string key, std::string &output);
@@ -95,10 +100,13 @@ int main(int argc, char** argv) {
     std::cout << "Encrypted  : " << output << std::endl;
 
     std::cout << "Writing file " << outputTextFile << std::endl;
-    if(writeOutputFile(output))
+
+    if (writeOutputFile(output)) {
         std::cout << "File was written" << std::endl;
-    else std::cout << "File was NOT written" << std::endl;
-    
+    } else {
+        std::cout << "File was NOT written" << std::endl;
+    }
+
 
     return 0;
 }
@@ -109,6 +117,7 @@ bool readInPlainText(std::string &sText) {
 
     if (myFile.is_open()) {
         while (std::getline(myFile, sText)) {
+
             out += sText;
         }
         myFile.close();
@@ -152,12 +161,20 @@ std::string trim(const std::string& str) {
     return str.substr(first, (last - first + 1));
 }
 
-void removeSpaces(std::string &s)
-{
-    std::string::iterator endIterator = std::remove(s.begin(),s.end(), ' ');
+void removeSpaces(std::string &s) {
+    std::string::iterator endIterator = std::remove(s.begin(), s.end(), ' ');
     s.erase(endIterator, s.end());
 }
 
+void removeLowercase(std::string &s) {
+    //    s.erase( std::remove_if(s.begin(),s.end(),[](unsigned char x){return std::islower(x);}) );
+    s.erase(std::remove_if(s.begin(),
+            s.end(),
+            [](unsigned char x) {
+                return std::islower(x);
+            }),
+    s.end());
+}
 
 bool writeOutputFile(std::string output) {
     std::ofstream f;
@@ -165,7 +182,7 @@ bool writeOutputFile(std::string output) {
     f.open(outputTextFile.c_str(), (std::ios::out | std::ios::trunc));
 
     if (f.is_open()) {
-        
+
         for (std::string::iterator it = output.begin();
                 it != output.end();
                 ++it) {
@@ -174,22 +191,22 @@ bool writeOutputFile(std::string output) {
         return true;
 
     }
-    
+
     return false;
 }
 
 void encrypt_S(std::string plain, std::string key, std::string &output) {
     unsigned int keyCount = 0;
     int alphabetSize = alphabet_S.size();
-    
+
     removeSpaces(plain);
-    
+
     for (std::string::iterator it = plain.begin(); it != plain.end(); ++it) {
         if (keyCount == key.size()) keyCount = 0;
 
         int alphaIndex = ((int) key[keyCount] - 65) + ((int) *it - 65);
-        
-        if (alphaIndex > (alphabetSize - 1) ) {
+
+        if (alphaIndex > (alphabetSize - 1)) {
             alphaIndex = alphaIndex - alphabetSize;
         }
 
@@ -202,9 +219,9 @@ void encrypt_S(std::string plain, std::string key, std::string &output) {
 void encrypt_L(std::string plain, std::string key, std::string &output) {
     unsigned int keyCount = 0;
     int alphabetSize = alphabet_L.size();
-    
+
     removeSpaces(plain);
-    
+
     for (std::string::iterator it = plain.begin(); it != plain.end(); ++it) {
         if (keyCount == key.size()) keyCount = 0;
 
@@ -212,7 +229,7 @@ void encrypt_L(std::string plain, std::string key, std::string &output) {
         //different ASCII values. See a table.
         int alphaIndex = ((int) key[keyCount] - 65) + ((int) *it - 65);
 
-        if (alphaIndex > (alphabetSize - 1) ) {
+        if (alphaIndex > (alphabetSize - 1)) {
             alphaIndex = alphaIndex - alphabetSize;
         }
 
@@ -222,8 +239,7 @@ void encrypt_L(std::string plain, std::string key, std::string &output) {
     }
 }
 
-bool writeFilesToTMP(void)
-{
-    
+bool writeFilesToTMP(void) {
+
     return true;
 }
